4c0553c5e1551945c3b928337326f5eb3ce98007
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-28 18:07:16 +0100

    fix:nav routes

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,16 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('active-quizzes')" :active="route().current('active-quizzes')">
+                                    Active Quizzes
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('create-quiz')" :active="route().current('create-quiz')">
+                                    Create Quiz
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -23,5 +20,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('active-quizzes', function (){
+    return Inertia::render('ActiveQuizzes'); // Assuming you have an ActiveQuizzes.vue component
+})->name('active-quizzes');
+
+Route::get('create-quiz', function (){
+    return Inertia::render('CreateQuiz'); // Assuming you have a CreateQuiz.vue component
+})->name('create-quiz');
 
 require __DIR__.'/auth.php';
