593ac7252e8de360d484f8de5e9aecc6ba8098f1
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-28 17:49:04 +0100

    fix:logo on auth user layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,10 +21,10 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
+                                <img src="/Q/1.png" alt="Logo" class="w-20 h-20" />
                                 </Link>
+
+
                             </div>
 
                             <!-- Navigation Links -->
