e31f4a34b966f74cf7fbfb1ce561fb544aecb79f
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-28 17:46:38 +0100

    fix: this and the previous commit was for guest layout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-200">
         <div>
             <Link href="/">
                 <img src="/Q/1.png" alt="Logo" class="w-20 h-20" />
