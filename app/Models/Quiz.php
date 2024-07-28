bda5c7f5d7b9bcdb42a90a4587206b27e7cf13d3
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-07-28 19:16:35 +0100

    user model and quiz Mm

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id', 'title', 'description'];
+
+    /**
+     * Get the user that owns the quiz.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
