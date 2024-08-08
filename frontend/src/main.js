e5fcd946c914a253f203bc2093c79540626b0a88
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-08-08 20:34:25 +0100

    omo

--- frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Import the router
-
-createApp(App)
-  .use(router) // Use the router
-  .mount('#app');
